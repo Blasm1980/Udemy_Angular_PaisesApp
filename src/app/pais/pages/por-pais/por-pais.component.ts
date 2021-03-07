f9282b2477ab0d4b940500727e495355483b66b9
Blasm1980 <blasm1980@gmail.com>
2021-03-07 16:13:26 +0100

    Fin Section 9

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { Country } from '../../interfaces/pais.interface';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
   ]
 })
 
@@ -15,11 +20,15 @@ export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
 
   constructor( private paisService: PaisService ) { }
 
   buscar( termino: string ) {
+
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino;
 
@@ -37,9 +46,22 @@ export class PorPaisComponent {
   }
 
   sugerencias ( termino: string ) {
+
+    this.mostrarSugerencias = true;
     this.hayError = false;
+    this.termino = termino;
 
+    this.paisService.buscarPais ( termino ) 
+      .subscribe( 
+          paises => this.paisesSugeridos = paises.splice(0,5),
+          (err) => this.paisesSugeridos = []
+      );
 
+  }
 
+  buscarSugerido( termino: string) {
+    this.buscar( termino );
+
+   
   }
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
@@ -10,6 +10,10 @@ export class PaisService {
 
   private apiUlr: string = 'https://restcountries.eu/rest/v2'
 
+  get httpParams () {
+     return new HttpParams().set('fields', 'name;capital;alpha2Code;flag;population'); 
+  }
+
 
   constructor( private http: HttpClient) { }
 
@@ -17,7 +21,7 @@ export class PaisService {
 
     const url = `${ this.apiUlr }/name/${ termino }`;
 
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
 
 
   }
@@ -25,7 +29,7 @@ export class PaisService {
 
     const url = `${ this.apiUlr }/capital/${ termino }`;
 
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
 
   }
 
@@ -36,5 +40,12 @@ export class PaisService {
     return this.http.get<Country>( url );
 
   }
+
+  buscarRegion( region: string ): Observable<Country[]> {
+
+    const url = `${ this.apiUlr }/region/${ region }`;
+
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
+  } 
 
 }
